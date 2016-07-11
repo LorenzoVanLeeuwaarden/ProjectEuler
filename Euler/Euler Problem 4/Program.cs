429ff1f1979a2ca3e22566fc8145c921b852b8dd
lorenzo van leeuwaarden <lorenzo van leeuwaarden>
2016-07-12 01:38:47 +0200

    Add Problem 4

---
+++ Euler/Euler Problem 4/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Euler_Problem_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int highestPalindrome = 0;
+            for (int a = 999; a >= 100; a--)
+            {
+                for (int b = 999; b >= 100; b--)
+                {
+                    if (IsPalindrome(a*b) && highestPalindrome < a*b)
+                    {
+                        highestPalindrome = a*b;
+                    }
+                }
+            }
+            Debug.WriteLine(highestPalindrome);
+        }
+
+        public static bool IsPalindrome(int number)
+        {
+            char[] chars = number.ToString().ToCharArray();
+            Array.Reverse(chars);
+            return number.ToString() == new string(chars);
+        }
+    }
+}
