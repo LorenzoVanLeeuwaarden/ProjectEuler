f88f4c870fcb44b351a0af47f03d6bfef8b7442f
lorenzo van leeuwaarden <lorenzo van leeuwaarden>
2016-07-12 02:25:18 +0200

    Add Problem 7

---
+++ Euler/Euler Problem 7/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Euler_Problem_7
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int primesfound = 1;
+            for (int i = 3; i < int.MaxValue; i = i + 2)
+            {
+                if (PrimeHandler.PrimeHandler.IsPrime(i)) primesfound++;
+                if (primesfound == 10001)
+                {
+                    Debug.WriteLine(i);
+                    break;
+                }
+            }
+        }
+    }
+}
