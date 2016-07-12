c3665aa1aa111d34aa049e35cb3a741a493f446c
lorenzo van leeuwaarden <lorenzo van leeuwaarden>
2016-07-12 04:49:05 +0200

    Add Problem 11

---
+++ Euler/Euler Problem 11/Program.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Euler_Problem_11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<List<int>> grid = TxtFileReader.TxtFileReader.ReadFile("grid.txt");
+            List<int> values = new List<int>
+            {
+                CheckVert(grid),
+                CheckDiagRight(grid),
+                CheckDiagLeft(grid),
+                CheckHor(grid)
+            };
+
+            Debug.WriteLine(values.Max());
+        }
+
+        private static int CheckDiagRight(List<List<int>> grid)
+        {
+            int sum = 0;
+            for (int y = 0; y < grid[0].Count - 3; y++)
+            {
+                for (int x = 0; x < grid[0].Count - 3; x++)
+                {
+                    var currentSum = grid[x][y] * grid[x + 1][y + 1] * grid[x + 2][y + 2] * grid[x + 3][y + 3];
+                    if (currentSum > sum)
+                    {
+                        sum = currentSum;
+                    }
+                }
+            }
+            return sum;
+        }
+
+        private static int CheckDiagLeft(List<List<int>> grid)
+        {
+            int sum = 0;
+            for (int y = 0; y < grid[0].Count - 3; y++)
+            {
+                for (int x = 19; x > 3; x--)
+                {
+                    var currentSum = grid[x][y] * grid[x - 1][y + 1] * grid[x - 2][y + 2] * grid[x - 3][y + 3];
+                    if (currentSum > sum)
+                    {
+                        sum = currentSum;
+                    }
+                }
+            }
+            return sum;
+        }
+
+        public static int CheckVert(List<List<int>> grid)
+        {
+            int sum = 0;
+            for (int y = 0; y < grid[0].Count - 3; y++)
+            {
+                for (int x = 0; x < grid[0].Count - 3; x++)
+                {
+                    var currentSum = grid[x][y]*grid[x + 1][y]*grid[x + 2][y]*grid[x + 3][y];
+                    if (currentSum > sum)
+                    {
+                        sum = currentSum;
+                    }
+                }
+            }
+            return sum;
+        }
+
+        public static int CheckHor(List<List<int>> grid)
+        {
+            int sum = 0;
+            for (int y = 0; y < grid[0].Count - 3; y++)
+            {
+                for (int x = 0; x < grid[0].Count - 3; x++)
+                {
+                    var currentSum = grid[y][x] * grid[y][x + 1] * grid[y][x + 2] * grid[y][x + 3];
+                    if (currentSum > sum)
+                    {
+                        sum = currentSum;
+                    }
+                }
+            }
+            return sum;
+        }
+
+    }
+}
---
+++ Euler/TxtFileReader/TxtFileReader.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace TxtFileReader
+{
+    public class TxtFileReader
+    {
+        static void Main(string[] args)
+        {
+
+        }
+
+        public static List<List<int>> ReadFile(string filepath)
+        {
+            string[] lines = System.IO.File.ReadAllLines(filepath);
+
+            return lines.Select(line => line.Split(' ')).Select(split => split.Select(number => Int32.Parse(number)).ToList()).ToList();
+        }
+    }
+}
