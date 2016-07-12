544e68f85e9fa9a5c4c40bda047a0f6f9084c17c
lorenzo van leeuwaarden <lorenzo van leeuwaarden>
2016-07-12 02:17:22 +0200

    Add Problem 6

---
+++ Euler/Euler Problem 6/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Euler_Problem_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double sumOfSquare = 0;
+            double squareOfSum = 0;
+            for (int i = 1; i <= 100; i++)
+            {
+                sumOfSquare += Math.Pow(i, 2);
+                squareOfSum += i;
+            }
+
+            Debug.WriteLine(Math.Pow(squareOfSum, 2) - sumOfSquare);
+        }
+    }
+}
