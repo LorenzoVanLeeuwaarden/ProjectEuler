776bdbe36cb5d18729d0c8f7fd09b95c415e48f1
lorenzo van leeuwaarden <lorenzo van leeuwaarden>
2016-07-12 03:58:05 +0200

    Add Problem 10

---
+++ Euler/Euler Problem 10/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+
+namespace Euler_Problem_10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            long sum = 0;
+            for (int i = 2; i < 2000000; i++)
+            {
+                if (PrimeHandler.PrimeHandler.IsPrime(i)) sum += i;
+            }
+            Debug.WriteLine(sum);
+        }
+    }
+}
