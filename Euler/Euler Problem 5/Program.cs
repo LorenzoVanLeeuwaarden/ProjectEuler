a99f2adbb31208edf0e386a51b34ac61a9d5e33c
lorenzo van leeuwaarden <lorenzo van leeuwaarden>
2016-07-12 02:01:45 +0200

    Add Problem 5

---
+++ Euler/Euler Problem 5/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Euler_Problem_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 2521; i < int.MaxValue; i++)
+            {
+                bool evenlyDivisible = true;
+                for (int b = 2; b < 20; b++)
+                {
+                    if (i%b != 0)
+                    {
+                        evenlyDivisible = false;
+                    }
+                }
+                if (evenlyDivisible)
+                {
+                    Debug.WriteLine(i);
+                    return;
+                }
+            }
+        }
+    }
+}
