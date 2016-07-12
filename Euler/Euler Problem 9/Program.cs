fa6a664559080cfd4e747db86a91d69b0fa9247f
lorenzo van leeuwaarden <lorenzo van leeuwaarden>
2016-07-12 03:46:20 +0200

    Add Problem 9

---
+++ Euler/Euler Problem 9/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Diagnostics;
+namespace Euler_Problem_9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int a = 1; a < 1000 / 3; a++)
+            {
+                for (int b = 1; b < 1000 / 2; b++)
+                {
+                    for (int c = 1; c < 1000; c++)
+                    {
+                        if (a*a + b*b == c*c && a + b + c == 1000)
+                        {
+                            Debug.WriteLine(a * b * c);
+                            return;
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
